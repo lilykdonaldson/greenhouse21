0f54dfee4e88da10c01b3914941f578b70b865fe
Lily Donaldson <lilykdonaldson@gmail.com>
2021-04-29 15:10:13 -0400

    add image

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
-let y = 100;
+let img;
+function preload() {
+  img = loadImage('assets/graphic.png');
+}
 
 function setup() {
-  createCanvas(720, 400);
+	if(windowWidth>800){
+		img.resize(windowWidth*0.6, 0);
+		createCanvas(windowWidth*0.94, img.height*1.1);
+	}
+	else{
+		img.resize(windowWidth*0.95, 0);
+		createCanvas(windowWidth, img.height*1.1);
+	}
+  background('grey');
+  
+  image(img, (windowWidth/2)-(img.width/1.7), 0);
 }
 
 function draw() {
-
+	ellipse(100,100,20,20)
 }
