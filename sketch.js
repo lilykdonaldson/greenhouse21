7af185329ff4e88d815edda66799d9352b6e7bf7
Lily Donaldson <lilykdonaldson@gmail.com>
2021-04-30 11:21:26 -0400

    working on popups

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let img;
+let popupMode = false;
 function preload() {
   img = loadImage('assets/graphic.png');
 }
@@ -30,66 +31,68 @@ function draw() {
 	ellipse(802,789,20,20)
 	strokeWeight(1.5);
 	stroke('#F4851F');
-	//Grow Lights Hover
-	let d = dist(mouseX, mouseY,342,227);
-  	if (d < 10) {
-  		line(342,227, 223,133);
-    	ellipse(342,227,25,25);
-    	rect(155,120,120,25);
-    	fill(255);
-    	strokeWeight(0);
-		text(' Grow Lights ➤ ', 215,137);
-		fill('#F4851F');
-		strokeWeight(1.5);
-  	}
-  	//Sensors Hover 
-	d = dist(mouseX, mouseY,276,343);
-  	if (d < 10) {
-  		line(276,343, 155,334);
-    	ellipse(276,343,25,25);
-    	rect(65,320,90,25);
-    	fill(255);
-    	strokeWeight(0);
-		text(' Sensors ➤ ', 112,338);
-		fill('#F4851F');
-		strokeWeight(1.5);
-  	}
-  	//Data Dashboard Hover
-	d = dist(mouseX, mouseY,836,243);
-  	if (d < 10) {
-    	line(836,243, 982,140);
-    	ellipse(836,243,25,25);
-    	rect(982,130,145,25);
-    	fill(255);
-    	strokeWeight(0);
-		text(' Data Dashboard ➤ ',1055,147);
-		fill('#F4851F');
-		strokeWeight(1.5);
-  	}
-    //Community Hover
-	d = dist(mouseX, mouseY,493,757);
-  	if (d < 10) {
-    	line(493,757, 221,674);
-    	ellipse(493,757,25,25);
-    	rect(221,674,120,25);
-    	fill(255);
-    	strokeWeight(0);
-		text(' Community ➤ ',283,692);
-		fill('#F4851F');
-		strokeWeight(1.5);
-  	}
-  	//Business Hover
-	d = dist(mouseX, mouseY,802,789);
-  	if (d < 10) {
-    	line(802,789, 892,664);
-    	ellipse(802,789,25,25);
-    	rect(892,654,120,25);
-    	fill(255);
-    	strokeWeight(0);
-		text(' Business ➤ ',954,672);
-		fill('#F4851F');
-		strokeWeight(1.5);
-  	}
+	if(!popupMode){
+		//Grow Lights Hover
+		let d = dist(mouseX, mouseY,342,227);
+	  	if (d < 10) {
+	  		line(342,227, 223,133);
+	    	ellipse(342,227,25,25);
+	    	rect(155,120,120,25);
+	    	fill(255);
+	    	strokeWeight(0);
+			text(' Grow Lights ➤ ', 215,137);
+			fill('#F4851F');
+			strokeWeight(1.5);
+	  	}
+	  	//Sensors Hover 
+		d = dist(mouseX, mouseY,276,343);
+	  	if (d < 10) {
+	  		line(276,343, 155,334);
+	    	ellipse(276,343,25,25);
+	    	rect(65,320,90,25);
+	    	fill(255);
+	    	strokeWeight(0);
+			text(' Sensors ➤ ', 112,338);
+			fill('#F4851F');
+			strokeWeight(1.5);
+	  	}
+	  	//Data Dashboard Hover
+		d = dist(mouseX, mouseY,836,243);
+	  	if (d < 10) {
+	    	line(836,243, 982,140);
+	    	ellipse(836,243,25,25);
+	    	rect(982,130,145,25);
+	    	fill(255);
+	    	strokeWeight(0);
+			text(' Data Dashboard ➤ ',1055,147);
+			fill('#F4851F');
+			strokeWeight(1.5);
+	  	}
+	    //Community Hover
+		d = dist(mouseX, mouseY,493,757);
+	  	if (d < 10) {
+	    	line(493,757, 221,674);
+	    	ellipse(493,757,25,25);
+	    	rect(221,674,120,25);
+	    	fill(255);
+	    	strokeWeight(0);
+			text(' Community ➤ ',283,692);
+			fill('#F4851F');
+			strokeWeight(1.5);
+	  	}
+	  	//Business Hover
+		d = dist(mouseX, mouseY,802,789);
+	  	if (d < 10) {
+	    	line(802,789, 892,664);
+	    	ellipse(802,789,25,25);
+	    	rect(892,654,120,25);
+	    	fill(255);
+	    	strokeWeight(0);
+			text(' Business ➤ ',954,672);
+			fill('#F4851F');
+			strokeWeight(1.5);
+	  	}
+	}
   	textSize(20);
   	fill('#474562');
   	strokeWeight(0);
@@ -99,7 +102,7 @@ function draw() {
   	fill('#6f6c91');
   	rect(515,1100,170,40);
   	fill(255);
-  	text('Whitepaper ➤',598,1126);
+  	text('Whitepaper',598,1126);
   	if (mouseX>515&&mouseX<(515+170)&&mouseY>1100&&mouseY<(1100+40)) {
   		stroke('#474562');
   		strokeWeight(8);
@@ -109,12 +112,22 @@ function draw() {
 	  	fill(255);
 	  	text('Whitepaper ➤',598,1126);
   	}
+  	if(popupMode){
+  		fill('rgba(10,10,10, 0.5)');
+  		strokeWeight(0);
+  		rect(0,0,1200,img.height*1.1);
+  		fill(0);
+  		var testrect = rect(300,windowHeight-(img.height*1.1),600,windowWidth);
+  		testrect.position(0, 0, 'fixed');
+
+  	}
 }
 function mousePressed() {
 	//Grow Lights Hit Point
 	let d = dist(mouseX, mouseY,342,227);
   	if (d < 10) {
-  		console.log("Grow Lights");
+  		popupMode = true;
+
   	}
   	//Sensors Hit Point
 	d = dist(mouseX, mouseY,276,343);
