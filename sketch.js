453a1a04ce14ac04f7e1bf7d99d77da1f8828ae0
Lily Donaldson <lilykdonaldson@gmail.com>
2021-05-03 10:08:48 -0400

    popup with text working

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let img;
 let popupMode = false;
 let pos = 25;
+let whoClicked;
 function preload() {
   img = loadImage('assets/graphic.png');
 }
@@ -18,7 +19,7 @@ function setup() {
 }
 
 function draw() {
-	background('grey');
+	background(240);
 	image(img, (1400/2)-(img.width/1.6), 0);
 	fill('#F4851F');
 	strokeWeight(1);
@@ -117,7 +118,7 @@ function draw() {
   		fill('rgba(10,10,10, 0.5)');
   		strokeWeight(0);
   		rect(0,0,1200,img.height*1.1);
-  		fill(0);
+  		fill(255);
   		if(pos<25){
   			pos=25;
   		}
@@ -125,7 +126,28 @@ function draw() {
   			pos = (img.height*1.1)-600;
   		}
   		rect(300,pos,600,550);
+  		fill(0);
+  		ellipse(875,pos+20,25,25);
+  		fill('red');
+  		text('X',875,pos+27);
+  		fill(0);
+  		textAlign(LEFT);
+  		if(whoClicked=="lights"){
+			let s = '    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum feugiat eros ut elit ultrices finibus. Fusce sagittis mi vitae enim interdum pharetra. Fusce maximus, dui quis tincidunt sagittis, massa est tincidunt mauris, et tempus leo ipsum nec sem. In non nisl consequat, lobortis magna eu, accumsan augue. Pellentesque tincidunt enim sit amet ipsum suscipit, in porta turpis commodo. Curabitur aliquam purus turpis, vel imperdiet augue luctus at. Duis ut semper sapien, et placerat sem. Aliquam posuere sem at sagittis dignissim. Donec vitae pulvinar neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In semper neque viverra sagittis rhoncus. Aenean rutrum lorem vel nulla molestie aliquam.\n   Donec venenatis, leo ut mattis ornare, justo magna tempor velit, vitae rutrum turpis arcu in ipsum. Duis ultricies rhoncus eleifend. Ut imperdiet finibus sapien interdum cursus. Etiam dui lectus, cursus eu quam sit amet, dictum gravida tellus. Phasellus nec tellus a neque venenatis imperdiet. Nullam eget augue dolor. Aenean in purus id tortor efficitur.';
+			text(s, 325,pos+35, 550, 525);
+  		}
+  		else if(whoClicked=="sensors"){
 
+  		}
+  		else if(whoClicked=="data"){
+  			
+  		}
+  		else if(whoClicked=="community"){
+  			
+  		}
+  		else if(whoClicked=="business"){
+  			
+  		}
   	}
 
 }
@@ -134,37 +156,45 @@ function mousePressed() {
 	let d = dist(mouseX, mouseY,342,227);
   	if (d < 10) {
   		popupMode = true;
+  		whoClicked = "lights";
 
   	}
   	//Sensors Hit Point
 	d = dist(mouseX, mouseY,276,343);
   	if (d < 10) {
-  		console.log("Sensors");
+  		popupMode = true;
+  		whoClicked = "sensors";
   	}
   	//Data Dashboard Hit Point
 	d = dist(mouseX, mouseY,836,243);
   	if (d < 10) {
-    	console.log("Data Dashboard");
+  		popupMode = true;
+    	whoClicked = "data";
   	}
     //Community Hit Point
 	d = dist(mouseX, mouseY,493,757);
   	if (d < 10) {
-    	console.log("Community");
+  		popupMode = true;
+    	whoClicked = "community";
   	}
   	//Business Hit Point
 	d = dist(mouseX, mouseY,802,789);
   	if (d < 10) {
-    	console.log("Business");
+  		popupMode = true;
+    	whoClicked = "business";
   	}
   	if (mouseX>515&&mouseX<(515+170)&&mouseY>1100&&mouseY<(1100+40)) {
   		window.open("https://www.google.com", '_blank');
   	}
+  	if(popupMode){
+  		d = dist(mouseX, mouseY,875,pos+20);
+  		if (d<12.5){
+  			popupMode = false;
+  		}
+  	}
+  	
 }
 
 function mouseWheel(event) {
-  //print(event.delta);
-  //move the square according to the vertical scroll amount
   pos += event.delta;
-  //uncomment to block page scrolling
-  //return false;
 }
